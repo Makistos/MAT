9a4eb9b217e968c0c4049e30a42d4223129d7185
Marko Poutiainen <mep@sofistes.net>
2012-03-07 10:21:52 +0200

    Added -b and -w to -d mode. Some code refactoring.

--- src/mat.py
+++ src/mat.py
@@ -8,10 +8,14 @@ import yaml
 import sys
 import getopt
 
+# Global variables. These are set in the main() function but accessed elsewhere.
+__baseAddress = "0"
+__bytesPerLine = 4
+
 def __usage():
     print '''Usage:
     -h, --help\tThis help text.
-    -b, --base\tBase address of memory dump.
+    -b, --baseAddress\tBase address of memory dump.
     -d, --diff\tDiff file. Generate a diff report between input file and diff file.
     -i, --input\tInput file (memory dump). Default: dump.reg.
     -m, --map\tRegister map file. Default: default.map.
@@ -21,24 +25,25 @@ def __usage():
     
     Default behaviour for the register offsets is that the memory dump 
     has been created starting from the address where the offsets are 
-    calculated. So base address would be 0 in this default case. If,
+    calculated. So baseAddress address would be 0 in this default case. If,
     however, only a part of memory is dumped, -b can be used to give
-    the base so that the offsets are fixed (argument is deducted from 
+    the baseAddress so that the offsets are fixed (argument is deducted from 
     the offsets).
     
     Register filter matches the string anywhere in the register's name. 
     
     Script will skip any registers that have offsets greater than size of
-    memory dump. Also, any registers where offset < base are naturally 
+    memory dump. Also, any registers where offset < baseAddress are naturally 
     skipped.
     
     A secondary mode is to use this as a diff tool that prints any differences between
     the input file (--input) and a diff file (--diff). This will print the offset and
     the values from both files. This is a handy way to figure which registers to add to
-    the map file. Other parameters are discarded in this mode.
+    the map file. -b and -w can be used in this mode.
     
     '''
     exit()
+
 
 def __testBit(val, bit):
     ''' Checks whether bit is on or off in val and returns "0" or "1" '''
@@ -57,7 +62,10 @@ def __bitRange(val, bits):
     else:
         # A range of bits
         retval = ''.join(map(lambda x: __testBit(val, x), reversed(range(int(bits[0]), int(bits[1])+1))))
-        # Could also leave reversed() out above and revese the result with retval[::-1], but this is more logical.
+        # Could also leave reversed() out above and reverse the result with retval[::-1], but this is more logical.
+        # Why reverse? Because the bits are defined as e.g. 0-4. If we didn't reverse, the resulting string of bits
+        # would start with bit 0, followed by bit 1 etc, which is opposite of the way you usually represent bit
+        # values.
         return retval
     
 def __printReg(regName, regDef, memory):
@@ -69,10 +77,9 @@ def __printReg(regName, regDef, memory):
         bits = regDef['Bits']
         for key, bit in sorted(bits.iteritems()):
             achar = ''
-            # Get bit range
-            bit_range = str(key).split('-', 2)
             # Generate bit pattern
-            bit_val = __bitRange(memory, bit_range)
+            bit_val = __bitRange(memory, str(key).split('-', 2))
+            # Get the extra description
             desc = bit.split('//')
             
             if len(bit_val) == 8:
@@ -89,9 +96,27 @@ def __printReg(regName, regDef, memory):
             if len(desc) > 1:
                 print '\t' + desc[1].strip()
     print
-    
-def __reportDifferences(mem1, mem2):
 
+def __createMemoryTriplet(lineNumber, val1, val2):
+    ''' 
+    Creates the memory triplet representing and registers that differ.
+    
+    The first parameter is converted into a hex value based on how many
+    bytes there are on a line and on the base address defined. 
+    '''
+    return [str(hex(lineNumber*__bytesPerLine) + __baseAddress), val1, val2]
+
+def __reportDifferences(mem1, mem2):
+    '''
+    Reports differences between the two memory sections. Will use first parameter as the 
+    reference unless second one is smaller in size.
+     
+    Returns an array where each item consists of three values: the address and the values
+    from each memory section.
+    '''
+    
+    retval = []
+     
     if len(mem1) != len(mem2):
         print "Memory sections have different size (%d - %d)" % (len(mem1), len(mem2))
         
@@ -104,22 +129,34 @@ def __reportDifferences(mem1, mem2):
         reg = mem1
         diff_reg = mem2
         
-    for idx, val in enumerate(reg):
-        if val != diff_reg[idx]:
-            print "Offset %s (%s - %s)" % (str(hex(idx*4)), val, diff_reg[idx])
+    # This code first selects the lines that differs with the filter() function and then simply creates
+    # an array of items that consists of three elements: the offset and the values from the two regions.
+    retval = map(lambda x: __createMemoryTriplet(x[0], x[1], diff_reg[x[0]]),  
+                 filter(lambda x: x[1] != diff_reg[x[0]], enumerate(reg)))
     
+    # This does the same as the below code:
+    #for idx, val in enumerate(reg):
+    #    if val != diff_reg[idx]:
+    #        retval.append([str(hex(idx*__bytesPerLine) + __baseAddress), val, diff_reg[idx]])
+    
+    return retval
+
+
+def __lineNumber(reg):
+    ''' Returns line number in the file based on the register address. '''
+    return (int(reg['Offset']) - int(__baseAddress, 16)) / __bytesPerLine
+
 def main(argv):
     
+    # Default values for parameters
     mapFile = 'default.map'
     inputFile = 'dump.reg'
-    base = "0"
     diffFile = ''
     register = ''
-    dataWidth = 4
     sortKey = 'Offset'
     # Read command-line options
     try:
-        opts, args = getopt.getopt(argv, 'm:i:b:d:r:s:w:h', ['map=','input=', 'base=', 'diff=', 'reg=', '--sort=', 'width=', 'help'])
+        opts, args = getopt.getopt(argv, 'm:i:b:d:r:s:w:h', ['map=','input=', 'baseAddress=', 'diff=', 'reg=', 'sort=', 'width=', 'help'])
     except getopt.GetoptError:
         __usage()
      
@@ -130,8 +167,8 @@ def main(argv):
             mapFile = arg
         if opt in ('-i', '--input'):
             inputFile = arg
-        if opt in ('-b', '--base'):
-            base = arg
+        if opt in ('-b', '--baseAddress'):
+            __baseAddress = arg
         if opt in ('-d', '--diff'):
             diffFile = arg
         if opt in ('-r', '--reg'):
@@ -142,7 +179,7 @@ def main(argv):
             else:
                 sortKey = arg
         if opt in ('-w', '--width'):
-            dataWidth = arg
+            __bytesPerLine = arg
     
     # Get register definition file
     f = file(mapFile, 'r')
@@ -163,15 +200,17 @@ def main(argv):
         for key, reg in sorted_list:
             # Trick here is that if no search string has been given it equates to '' which matches every register
             if 'Offset' in reg and key.find(register) != -1:
-            # Offset is in bytes and each line contains four bytes:
-                line = (int(reg['Offset'])-int(base, 16)) / dataWidth
+                # Offset is in bytes and each line contains four bytes:
+                line = __lineNumber(reg)
                 # Skip any registers that are before or after the range of the dump
                 if len(memory) >= line and line >= 0:
                     __printReg(key, reg, memory[line])
     else:
         # Diff mode, tell differences between the two files
         memory2 = [line.strip() for line in open(diffFile) if line.startswith('0x')]
-        __reportDifferences(memory, memory2)
+        diffs = __reportDifferences(memory, memory2)
+        for L in diffs:
+            print 'Offset %s (%s - %s)' % (L[0], L[1], L[2])
         
 if __name__ == '__main__':
     
